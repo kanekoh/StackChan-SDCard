8060e49352e0028110cb051380160a0a1533c194
Hiroyuki Kaneko <neconoko@gmail.com>
2025-05-25 17:00:00 +0900

    Add read/write Json

--- src/SDUtils.cpp
+++ src/SDUtils.cpp
@@ -1,5 +1,6 @@
 #include "SDUtils.h"
 #include <SD.h>
+#include <ArduinoJson.h>
 
 bool initSDCard() {
   if (!SD.begin(GPIO_NUM_4, SPI, 25000000)) {
@@ -27,5 +28,41 @@ bool readLinesFromSD(const char* path, std::vector<String>& lines) {
   }
 
   file.close();
+  return true;
+}
+
+
+bool writeJsonToSD(const char* path, const DynamicJsonDocument& doc) {
+  File file = SD.open(path, FILE_WRITE);
+  if (!file) {
+    Serial.printf("Failed to open file for writing: %s\n", path);
+    return false;
+  }
+
+  if (serializeJson(doc, file) == 0) {
+    Serial.printf("Failed to write JSON to file: %s\n", path);
+    file.close();
+    return false;
+  }
+
+  file.close();
+  return true;
+}
+
+bool readJsonFromSD(const char* path, DynamicJsonDocument& doc) {
+  File file = SD.open(path, FILE_READ);
+  if (!file) {
+    Serial.printf("Failed to open file for reading: %s\n", path);
+    return false;
+  }
+
+  DeserializationError error = deserializeJson(doc, file);
+  file.close();
+
+  if (error) {
+    Serial.printf("Failed to parse JSON from file: %s\n", path);
+    return false;
+  }
+
   return true;
 }
--- src/SDUtils.h
+++ src/SDUtils.h
@@ -4,3 +4,5 @@
 
 bool initSDCard();
 bool readLinesFromSD(const char* path, std::vector<String>& lines);
+bool writeJsonToSD(const char* path, const DynamicJsonDocument& doc);
+bool readJsonFromSD(const char* path, DynamicJsonDocument& doc);
